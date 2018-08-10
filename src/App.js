08ea46640eed41bad40c0477cc4b1e48fb669600
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2018-08-10 01:30:09 -0500

    create prototype component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import PrototypeSample from './assets/components/prototype/prototype.js'
+import './App.css';
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <PrototypeSample></PrototypeSample>
+      </div>
+    );
+  }
+}
+
+export default App;
